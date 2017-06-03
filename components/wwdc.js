d7400fca07f970eae0ecbaf2a930f14cb1d10763
Jiacheng Jiang <ajax15601560911@gmail.com>
2017-06-03 16:05:17 -0400

    updated response ui

--- components/wwdc.js
+++ components/wwdc.js
@@ -8,11 +8,10 @@ var wwdcSchema = new Schema({
         required: true
     }
 })
+
 mongoose.connect("mongodb://utzhushou:ada23333@ds019836.mlab.com:19836/utzhushou")
-// var URL_OF_LIBRARY_API = "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0\u0026amp;num=-1\u0026amp;q=http://www.feed43.com/2343747281410148.xml";
 
 wwdcSchema.statics.findRecentOne = function(cb, id) {
-  // return this.findOne({_id: { $ne: id }}, cb);
   return this.find({_id: { $ne: id }}, cb).limit(1).sort({$natural:-1})
 };
 
@@ -37,7 +36,7 @@ exports.wwdc = function(query, callback) {
                 } else {
                     var response = "正在给你呈现最近的" + data.length + "条回复！\n\n"
                     data.forEach(line => response += ("----------\n" + line.content + '\n'))
-                    response += "----------\n请继续多多回复！(ง๑ •̀_•́)ง"
+                    response += "==========\n请继续多多回复！(ง๑ •̀_•́)ง"
                     return callback(response);
                 }
             })
@@ -56,7 +55,7 @@ exports.wwdc = function(query, callback) {
                             console.log(err);
                             return callback("呃、服务器出问题了，我们好像听不清你在说什么。\n等会再试试吧，ADA的技术人员马上睡醒了~")
                         } else {
-                            var response = "你的回复我确实听到了！\n(o゜▽゜)o☆\n最近的一条发言是\n\n『" + data[0].content +"』\n\n==========\n直接回复wwdc可以查看最新的10条哟"
+                            var response = "你的回复我确实听到了！\n(o゜▽゜)o☆\n最近的一条发言是\n\n----------\n" + data[0].content + "\n----------\n直接回复wwdc可以查看最新的10条哟"
                             return callback(response);
                         }
                     }, newComment._id)
